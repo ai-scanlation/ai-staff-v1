18a84d320382ce65670f58c1476a33ff4a8e8af3
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2017-11-22 20:49:28 +0700

    Move Explorer data to State

--- script/state.js
+++ script/state.js
@@ -10,6 +10,43 @@ module.exports = {
         defaultFolder_1: '',
         defaultFolder_2: '',
         defaultFolder_3: '',
-        defaultFolder_4: ''
+        defaultFolder_4: '',
+
+        explorerMode: '',
+        explorerLimit: true,
+
+        renamePattern: '[Ai Scans] Shiki Chap <15> - <0+><ext>',
+        renameProjectID: 1,
+        renameNumber: 1,
+        renameNumberString: '001',
+        renameIndex: 1,
+
+        convertExtensionID: 0,
+        convertWidthID: 1,
+        convertInterpolateID: 0,
+        customConvertWidth: 800,
+
+        allowConvertExtension: [
+            'PNG',
+            'JPG'
+        ],
+        allowConvertWidth: [
+            '',
+            '100%'
+        ],
+        allowConvertInterpolate: [
+            'Average',
+            'Average4',
+            'Average9',
+            'Average16',
+            'Background',
+            'Bilinear',
+            'Blend',
+            'Catrom',
+            'Integer',
+            'Mesh',
+            'Nearest',
+            'Spline'
+        ]
     }
 };
