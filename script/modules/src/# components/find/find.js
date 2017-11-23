04b89036564ac13946435a97d456eab489234585
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2017-11-23 17:26:34 +0700

    Module: new way to find: '#<module>.<memberFunction>'
    Example: 
    new: find('#picasa.getCodeAndToken')
    old: find('#.picasa')

--- script/modules/src/# components/find/find.js
+++ script/modules/src/# components/find/find.js
@@ -1,8 +1,5 @@
 import recursiveFind from './src/recursiveFind.js';
 module.exports = function find(findPath) {
-    // if (findPath === "scroll") {
-    //     throw findPath;
-    // }
     if (arguments.length >= 3) {
         const arg = [...arguments].slice(1);
         return find(findPath, find(...arg));
@@ -20,8 +17,14 @@ module.exports = function find(findPath) {
     const callback = typeof arguments[1] === 'function' ? arguments[1] : () => {};
     if (name[0] === '#') {
         const modules = require('modules');
-        callback(modules, property);
-        return [modules, property];
+        const moduleName = name.substr(1);
+        if (moduleName == '') {
+            callback(modules, property);
+            return [modules, property];
+        } else {
+            callback(modules[moduleName], property);
+            return [modules[moduleName], property];
+        }
     } else if (name || ref) {
         const where = (typeof arguments[1] === 'string') ? [find(arguments[1])]
             : (Array.isArray(arguments[1]) ? [arguments[1][0]]
